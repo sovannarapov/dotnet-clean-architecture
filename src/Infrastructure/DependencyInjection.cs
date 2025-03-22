949d16ca1527d894a623728819450d7c9afce0e2
sovannarapov <sovannarapov9@gmail.com>
2025-03-22 19:27:54 +0700

    refactor: update DependencyInjection.cs

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,6 +1,31 @@
-﻿namespace Infrastructure;
+﻿using Application.Abstractions.Data;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
 
 public static class DependencyInjection
 {
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration) =>
+        services
+            .AddDatabase(configuration);
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("Database");
 
+        services.AddDbContext<ApplicationDbContext>(
+            options => options
+                .UseNpgsql(connectionString, npgsqlOptions =>
+                    npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Default))
+                .UseSnakeCaseNamingConvention());
+
+        services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        return services;
+    }
 }
