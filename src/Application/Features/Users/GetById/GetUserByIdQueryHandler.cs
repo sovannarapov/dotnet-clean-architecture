bb76dba6f4b3293de98bc5c28a425811d390dc91
sovannarapov <sovannarapov9@gmail.com>
2025-03-22 20:27:25 +0700

    feat: add GetUserByIdQueryHandler.cs

---
+++ src/Application/Features/Users/GetById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Dtos.User;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Features.Users.GetById;
+
+internal sealed class GetUserByIdQueryHandler(IApplicationDbContext context, IUserContext userContext)
+    : IQueryHandler<GetUserByIdQuery, UserDto>
+{
+    public async Task<Result<UserDto>> Handle(GetUserByIdQuery query, CancellationToken cancellationToken)
+    {
+        if (query.UserId != userContext.UserId)
+        {
+            return Result.Failure<UserDto>(UserErrors.Unauthorized());
+        }
+
+        UserDto? user = await context.Users
+            .Where(u => u.Id == query.UserId)
+            .Select(u => new UserDto
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Email = u.Email
+            })
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (user is null)
+        {
+            return Result.Failure<UserDto>(UserErrors.NotFound(query.UserId));
+        }
+
+        return user;
+    }
+}
