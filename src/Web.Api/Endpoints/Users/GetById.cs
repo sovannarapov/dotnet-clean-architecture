9654806e7c086e9d137bf8e00c564d699d88ccbb
sovannarapov <sovannarapov9@gmail.com>
2025-03-22 20:27:48 +0700

    feat: add GetById.cs

---
+++ src/Web.Api/Endpoints/Users/GetById.cs
@@ -0,0 +1,25 @@
+﻿using Application.Dtos.User;
+using Application.Features.Users.GetById;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Users;
+
+internal sealed class GetById : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("users/{userId}", async (Guid userId, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetUserByIdQuery(userId);
+
+            Result<UserDto> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .HasPermission(Permissions.UsersAccess)
+        .WithTags(Tags.Users);
+    }
+}
