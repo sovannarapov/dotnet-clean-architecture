5d16879f61520e8ea79e85215ebab0c5d3ec3b98
sovannarapov <sovannarapov9@gmail.com>
2025-03-22 20:26:55 +0700

    feat: add Login.cs

---
+++ src/Web.Api/Endpoints/Users/Login.cs
@@ -0,0 +1,24 @@
+﻿using Application.Dtos.User;
+using Application.Features.Users.Login;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Users;
+
+internal sealed class Login : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("users/login", async (LoginRequest request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new LoginUserCommand(request.Email, request.Password);
+
+            Result<string> result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .WithTags(Tags.Users);
+    }
+}
