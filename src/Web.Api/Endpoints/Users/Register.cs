843144fa767222ed9d7f6f1157001bf0c9cc7f62
sovannarapov <sovannarapov9@gmail.com>
2025-03-22 19:43:14 +0700

    feat: add Register.cs

---
+++ src/Web.Api/Endpoints/Users/Register.cs
@@ -0,0 +1,28 @@
+﻿using Application.Dtos.User;
+using Application.Features.Users.Register;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Users;
+
+internal sealed class Register : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("users/register", async (RegisterRequest request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new RegisterUserCommand(
+                request.Email,
+                request.FirstName,
+                request.LastName,
+                request.Password);
+
+            Result<Guid> result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .WithTags(Tags.Users);
+    }
+}
