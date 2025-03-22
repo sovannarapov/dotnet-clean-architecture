bf5cceb1425660add8a49cf5e60c15b1c015780a
sovannarapov <sovannarapov9@gmail.com>
2025-03-22 19:26:05 +0700

    feat: add UserDto.cs

---
+++ src/Application/Dtos/User/UserDto.cs
@@ -0,0 +1,9 @@
+namespace Application.Dtos.User;
+
+public record UserDto
+{
+    public Guid Id { get; init; }
+    public string Email { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+}
