de4ae46922e1cafd7e8766e01652ef6c2ca57a29
sovannarapov <sovannarapov9@gmail.com>
2025-03-22 20:28:20 +0700

    feat: add GetUserByEmailQueryHandler.cs

---
+++ src/Application/Features/Users/GetByEmail/GetUserByEmailQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Dtos.User;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Features.Users.GetByEmail;
+
+internal sealed class GetUserByEmailQueryHandler(IApplicationDbContext context, IUserContext userContext)
+    : IQueryHandler<GetUserByEmailQuery, UserDto>
+{
+    public async Task<Result<UserDto>> Handle(GetUserByEmailQuery query, CancellationToken cancellationToken)
+    {
+        UserDto? user = await context.Users
+            .Where(u => u.Email == query.Email)
+            .Select(u => new UserDto
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Email = u.Email
+            })
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (user is null)
+        {
+            return Result.Failure<UserDto>(UserErrors.NotFoundByEmail);
+        }
+
+        if (user.Id != userContext.UserId)
+        {
+            return Result.Failure<UserDto>(UserErrors.Unauthorized());
+        }
+
+        return user;
+    }
+}
