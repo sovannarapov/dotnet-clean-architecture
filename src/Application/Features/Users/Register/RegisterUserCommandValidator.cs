0d9f7050d0500ade43264e0fdf7f4907f51fe3cf
sovannarapov <sovannarapov9@gmail.com>
2025-03-25 16:27:42 +0700

    refactor: update RegisterUserCommandValidator.cs

--- src/Application/Features/Users/Register/RegisterUserCommandValidator.cs
+++ src/Application/Features/Users/Register/RegisterUserCommandValidator.cs
@@ -6,9 +6,25 @@ internal sealed class RegisterUserCommandValidator : AbstractValidator<RegisterU
 {
     public RegisterUserCommandValidator()
     {
-        RuleFor(c => c.FirstName).NotEmpty();
-        RuleFor(c => c.LastName).NotEmpty();
-        RuleFor(c => c.Email).NotEmpty().EmailAddress();
-        RuleFor(c => c.Password).NotEmpty().MinimumLength(8);
+        RuleFor(c => c.FirstName)
+            .NotEmpty()
+            .MaximumLength(50)
+            .WithMessage("First name must not exceed 50 characters");
+
+        RuleFor(c => c.LastName)
+            .NotEmpty()
+            .MaximumLength(50)
+            .WithMessage("Last name must not exceed 50 characters");
+
+        RuleFor(c => c.Email)
+            .NotEmpty()
+            .WithMessage("Email is required")
+            .EmailAddress()
+            .WithMessage("Invalid email format");
+
+        RuleFor(c => c.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .WithMessage("Password must be at least 8 characters long");
     }
 }
