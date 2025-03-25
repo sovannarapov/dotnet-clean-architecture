d9bfc02086ef91b2a6326c01730088be4f9bd7a6
sovannarapov <sovannarapov9@gmail.com>
2025-03-25 16:29:00 +0700

    feat: add UpdateUserCommandValidator.cs

---
+++ src/Application/Features/Users/Update/UpdateUserCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Users.Update;
+using FluentValidation;
+
+namespace Application.Features.Users.Register;
+
+internal sealed class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+{
+    public UpdateUserCommandValidator()
+    {
+        RuleFor(c => c.UserId)
+            .NotEmpty()
+            .WithMessage("UserId is required");
+
+        RuleFor(c => c.FirstName)
+            .NotEmpty()
+            .WithMessage("First name is required")
+            .MaximumLength(50)
+            .WithMessage("First name must not exceed 50 characters");
+
+        RuleFor(c => c.LastName)
+            .NotEmpty()
+            .WithMessage("Last name is required")
+            .MaximumLength(50)
+            .WithMessage("Last name must not exceed 50 characters");
+
+        RuleFor(c => c.Email)
+            .NotEmpty()
+            .WithMessage("Email is required")
+            .EmailAddress()
+            .WithMessage("Email is not valid");
+    }
+}
