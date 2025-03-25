c970ec859f28cd1a108c0fdc9968ecd10aa73abe
sovannarapov <sovannarapov9@gmail.com>
2025-03-25 19:45:10 +0700

    refactor: update UpdateUserCommandValidator.cs

--- src/Application/Features/Users/Update/UpdateUserCommandValidator.cs
+++ src/Application/Features/Users/Update/UpdateUserCommandValidator.cs
@@ -11,22 +11,22 @@ internal sealed class UpdateUserCommandValidator : AbstractValidator<UpdateUserC
             .NotEmpty()
             .WithMessage("UserId is required");
 
-        RuleFor(c => c.FirstName)
-            .NotEmpty()
-            .WithMessage("First name is required")
-            .MaximumLength(50)
-            .WithMessage("First name must not exceed 50 characters");
+        When(c => c.FirstName is not null, () => RuleFor(c => c.FirstName)
+                .NotEmpty()
+                .WithMessage("First name cannot be empty")
+                .MaximumLength(50)
+                .WithMessage("First name must not exceed 50 characters"));
 
-        RuleFor(c => c.LastName)
+        When(c => c.LastName is not null, () => RuleFor(c => c.LastName)
             .NotEmpty()
             .WithMessage("Last name is required")
             .MaximumLength(50)
-            .WithMessage("Last name must not exceed 50 characters");
+            .WithMessage("Last name must not exceed 50 characters"));
 
-        RuleFor(c => c.Email)
+        When(c => c.Email is not null, () => RuleFor(c => c.Email)
             .NotEmpty()
             .WithMessage("Email is required")
             .EmailAddress()
-            .WithMessage("Email is not valid");
+            .WithMessage("Email is not valid"));
     }
 }
