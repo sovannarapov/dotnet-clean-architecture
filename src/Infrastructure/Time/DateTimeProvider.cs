4287934ecc2fdaa49a4311869161bb71a06f6ec0
sovannarapov <sovannarapov9@gmail.com>
2025-04-07 09:02:52 +0700

    refactor: improve DateTimeProvider.cs

--- src/Infrastructure/Time/DateTimeProvider.cs
+++ src/Infrastructure/Time/DateTimeProvider.cs
@@ -2,7 +2,12 @@
 
 namespace Infrastructure.Time;
 
-internal sealed class DateTimeProvider : IDateTimeProvider
+internal sealed class DateTimeProvider : TimeProvider
 {
-    public DateTime UtcNow => DateTime.UtcNow;
+    public override DateTimeOffset GetUtcNow() => DateTimeOffset.UtcNow;
+
+    public override ITimer CreateTimer(TimerCallback callback, object? state, TimeSpan dueTime, TimeSpan period)
+    {
+        return new Timer(callback, state, dueTime, period);
+    }
 }
